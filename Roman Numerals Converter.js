c21d210456ca7807400468b289f3339149299b4f
Aleksandar Krstić <acckrs@gmail.com>
2017-02-21 00:20:31 +0100

    Revert "First working version of this task"
    This reverts commit 92c5b0c2fa3c30b47239c9d35826e14f6f22ee56.

--- Roman Numerals Converter.js
+++ Roman Numerals Converter.js
@@ -1,50 +1,24 @@
+
 function convertToRoman(num) {
-  var matrix = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X",
-    20: "XX",
-    30: "XXX",
-    40: "XL",
-    50: "L",
-    60: "LX",
-    70: "LXX",
-    80: "LXXX",
-    90: "XC",
-    100: "C",
-    200: "CC",
-    300: "CCC",
-    400: "CD",
-    500: "D",
-    600: "DC",
-    700: "DCC",
-    800: "DCCC",
-    900: "CM",
-    1000: "M",
-    2000: "MM",
-    3000: "MMM"
-  };
-  var digits = num.toString().split('');
-  var newArr = [];
-  var digitsCount = num.toString().length,
-    counter = 0,
-    exponent = digitsCount - 1;
-  while (counter < digitsCount) {
-    digits[counter]=(digits[counter] * Math.pow(10, exponent));
+  var matrix={
+  1:"I",2:"II",3:"III", 4:"IV",5:"V",6:"VI",  7:"VII",8:"VIII", 9:"IX",
+  10:"X",20:"XX",30:"XXX",40:"XL",50:"L",60:"LX",70:"LXX",80:"LXXX",90:"XC",
+  100:"C",200:"CC",300:"CCC",400:"CD",500:"D",600:"DC",700:"DCC",800:"DCCC",900:"CM",
+  1000:"M",2000:"MM",3000:"MMM"
+};
+  var cifre=num.toString().split('');
+  var newArr=[];
+  var brojCifara=num.toString().length,counter=0,eksponent=brojCifara-1;
+  while(counter<brojCifara){
+    newArr.push(cifre[counter]*Math.pow(10,eksponent));
     counter++;
-    exponent--;
+    eksponent--;
   }
-  var romArr = digits.map(function (val) {
+  var romArr=newArr.map(function(val){
     return matrix[val];
   });
-  return romArr.join('');
+  
+  return romArr.join('') ;
 }
 
 console.log(convertToRoman(3336));

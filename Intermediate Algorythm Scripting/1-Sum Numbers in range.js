638cc8e6776c4461a0dda905b16ecbf3309d62c7
Aleksandar Krstic <akrstic@GrandCasino.local>
2017-02-21 10:52:08 +0100

    i

---
+++ Intermediate Algorythm Scripting/1-Sum Numbers in range.js
@@ -0,0 +1,21 @@
+/*
+We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+The lowest number will not always come first.
+*/
+
+function sumAll(arr) {
+    var values = [];
+    var maxV = Math.max.apply(null, arr);
+    var minV = arr.reduce(function(a, b) {
+        return Math.min(a, b);
+    });
+    for (var i = minV; i <= maxV; i++) {
+        values.push(i);
+    }
+
+    return (values.reduce(function(a, b) {
+        return a + b;
+    }));
+}
+
+console.log(sumAll([6, 4]));

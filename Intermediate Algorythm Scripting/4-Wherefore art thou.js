c7362925c60de6fe651458805774f84219b082a1
Aleksandar Krstić <acckrs@gmail.com>
2017-02-25 22:07:42 +0100

    o

--- Intermediate Algorythm Scripting/4-Wherefore art thou.js
+++ Intermediate Algorythm Scripting/4-Wherefore art thou.js
@@ -1,22 +1,17 @@
 function whatIsInAName(collection, source) {
     // What's in a name?
-    var arr = [];
-    // Only change code below this line
-
-
-    // Only change code above this line
-    return arr;
+        var sourceKeys=Object.keys(source);
+        function check(item){
+            for (var i=0;i<sourceKeys.length;i++){
+                if (!item.hasOwnProperty(sourceKeys[i])||item[sourceKeys[i]]!==source[sourceKeys[i]])
+                return false;
+            }
+            return true;
+        }
+   return console.log(collection.filter(check));
 }
 
-whatIsInAName([{
-    first: "Romeo",
-    last: "Montague"
-}, {
-    first: "Mercutio",
-    last: null
-}, {
-    first: "Tybalt",
-    last: "Capulet"
-}], {
-    last: "Capulet"
-});
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 });
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 });

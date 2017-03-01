73a77524d7cea974f2908e71fc68f371cf3e69c8
Aleksandar Krstić <acckrs@gmail.com>
2017-03-01 12:16:27 +0100

    regex challenge

---
+++ Intermediate Algorythm Scripting/10-Convert HTML Entities.js
@@ -0,0 +1,11 @@
+var maps = {
+  "&": "&​amp;",
+  "<": "&​lt;",
+  ">": "&​gt;",
+  "'": "&​apos;",
+};
+var str = "Dolce '&\"<> Gabanna";
+var newStr=str.replace(/'|&|<|>/g,function(match){
+  return maps[match];
+}).replace(/\"/g,"&quot;");
+console.log(newStr);
